3119f601a9770a69e784f7ed5d8bae83fc1bc2fb
tydonnelly11 <tydonnelly11@gmail.com>
2023-10-31 18:53:41 -0500

    added prettier for code formating, check readme for details

--- src/components/WeekDropdown.vue
+++ src/components/WeekDropdown.vue
@@ -1,32 +1,36 @@
 <template>
-    <div class="dropdown-center" >
-  <a class="btn btn-primary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" :style="'width: 30%;'">
-    Weeks
-  </a>
+  <div class="dropdown-center">
+    <a
+      class="btn btn-primary dropdown-toggle"
+      href="#"
+      role="button"
+      data-bs-toggle="dropdown"
+      aria-expanded="false"
+      :style="'width: 30%;'"
+    >
+      Weeks
+    </a>
 
-  <ul class="dropdown-menu" :style="'columns : 3;'">
-    <li v-for="date in dates"><a class="dropdown-item" href="#" >{{ date }}</a></li> 
-    
-  </ul>
-</div>
-
+    <ul class="dropdown-menu" :style="'columns : 3;'">
+      <li v-for="date in dates">
+        <a class="dropdown-item" href="#">{{ date }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'WeekDropdown',
-    props: {
-        dates: Array
-    },
+  name: "WeekDropdown",
+  props: {
+    dates: Array,
+  },
 
-    //Note for future self while doing visual only, so i think I 
-    //should try to emit event to warList which I think will handle what week is
-    //in the little row as well as adding rows 
-    //also look at bootstrap docs
-    
-}
-
+  //Note for future self while doing visual only, so i think I
+  //should try to emit event to warList which I think will handle what week is
+  //in the little row as well as adding rows
+  //also look at bootstrap docs
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/StudentHomePageView.vue
+++ src/views/StudentHomePageView.vue
@@ -1,33 +1,32 @@
 <template>
-      <div class="sidebar">
-        <NavbarSide/></div>
-      
-      <div class="main-item">
-      <router-view>
-      </router-view>
-      </div>
+  <div class="sidebar">
+    <NavbarSide />
+  </div>
+
+  <div class="main-item">
+    <router-view> </router-view>
+  </div>
 </template>
 
 <script>
-import NavbarSide from '../components/NavbarSide.vue';
-export default{
-    name: 'StudentHomePageView',
-    components: {
-        NavbarSide
-    }
-}
-
+import NavbarSide from "../components/NavbarSide.vue";
+export default {
+  name: "StudentHomePageView",
+  components: {
+    NavbarSide,
+  },
+};
 </script>
 
 <style scoped>
-.sidebar{
+.sidebar {
   width: 25%;
-    margin-left: 3.5%;
-
+  margin-left: 3.5%;
 }
 
-.main-item{
+.main-item {
   width: 100%;
   display: flex;
   flex-direction: column;
-  }</style>
+}
+</style>
